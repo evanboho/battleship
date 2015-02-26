ffa591cb64a3e9ccec287dfd361fa6ba677aae9c
Jason Ku <jku@threadflip.com>
2015-02-26 13:55:25 -0800

    Add Board#guess method.
    * Space#open? and #boat? methods.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,19 @@
 class Board < ActiveRecord::Base
   has_many :spaces
+
+  def guess(letter, number)
+    space = spaces.guessable_only.find_by_letter_and_number(letter, number)
+
+    if space.blank?
+      raise "No guessable spaces at #{letter} #{number}"
+    elsif space.open?
+      space.update_attributes(state: 'guessed')
+
+      false
+    elsif space.boat?
+      space.update_attributes(state: 'hit')
+
+      true
+    end
+  end
 end
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,3 +1,17 @@
 class Space < ActiveRecord::Base
   belongs_to :board
+
+  # States
+  # boat, guessed, and hit
+  # null => open
+
+  scope :guessable_only, -> { where("state IS NULL or state = 'boat'") }
+
+  def open?
+    state.nil?
+  end
+
+  def boat?
+    state == 'boat'
+  end
 end
