760ce31a099f03cc6943474d754529f0532788b2
evanboho <evan.kuchar@gmail.com>
2015-02-26 13:42:11 -0800

    add relationships to board and space

--- db/migrate/20150226213900_create_spaces.rb
+++ db/migrate/20150226213900_create_spaces.rb
@@ -3,9 +3,14 @@ class CreateSpaces < ActiveRecord::Migration
     create_table :spaces do |t|
       t.integer :board_id
       t.string :state
+      t.string :letter
+      t.string :number
 
       t.timestamps null: false
     end
+    add_index :spaces, :letter
+    add_index :spaces, :number
     add_index :spaces, :board_id
+
   end
 end
